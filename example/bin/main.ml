5879b243d71190728786926935051f50759d375c
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-11-01 20:13:24 +0100

    fix -version

--- example/bin/main.ml
+++ example/bin/main.ml
@@ -1,1 +1,7 @@
-let () = Command_unix.run Example.main
+let () =
+  Command_unix.run
+  (* The following vars are substituted during the `dune-release` process. *)
+    ~version:"%%VERSION%%"
+    ~build_info:"%%VCS_COMMIT_ID%%"
+    Example.main
+;;
--- template/bin/main.ml
+++ template/bin/main.ml
@@ -1,1 +1,7 @@
-let () = Command_unix.run {{ project_snake | capitalize }}.main
+let () =
+  Command_unix.run
+  (* The following vars are substituted during the `dune-release` process. *)
+    ~version:"%%VERSION%%"
+    ~build_info:"%%VCS_COMMIT_ID%%"
+    {{ project_snake | capitalize }}.main
+;;
