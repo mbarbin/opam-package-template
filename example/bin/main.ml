0bd525b13cbff2fa9d39b5f3ab111c82d9561a59
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-11-03 14:04:52 +0100

    use command-unix-for-opam

--- example/bin/main.ml
+++ example/bin/main.ml
@@ -1,7 +1,1 @@
-let () =
-  Command_unix.run
-  (* The following vars are substituted during the `dune-release` process. *)
-    ~version:"%%VERSION%%"
-    ~build_info:"%%VCS_COMMIT_ID%%"
-    Example.main
-;;
+let () = Command_unix_for_opam.run Example.main
--- template/bin/main.ml
+++ template/bin/main.ml
@@ -1,7 +1,1 @@
-let () =
-  Command_unix.run
-  (* The following vars are substituted during the `dune-release` process. *)
-    ~version:"%%VERSION%%"
-    ~build_info:"%%VCS_COMMIT_ID%%"
-    {{ project_snake | capitalize }}.main
-;;
+let () = Command_unix_for_opam.run {{ project_snake | capitalize }}.main
